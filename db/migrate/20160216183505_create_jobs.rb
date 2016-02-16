9fa0d8815e5a0633137b66778dcc91a869b16207
Holly-jolly <holly.haixin.l@gmail.com>
2016-02-16 10:43:13 -0800

    create jobs tble

--- db/migrate/20160216183505_create_jobs.rb
+++ db/migrate/20160216183505_create_jobs.rb
@@ -1,6 +1,21 @@
 class CreateJobs < ActiveRecord::Migration
   def change
-    create_table :jobs do |t|
+    create_table :jobs, id: :uuid do |t|
+      t.string   "name"
+      t.text     "description"
+      t.date     "starts_on"
+      t.date     "ends_on"
+      t.string   "location"
+      t.string   "contact_name"
+      t.string   "contact_phone"
+      t.string   "contact_email"
+      t.string   "company_name"
+      t.string   "secret"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.boolean  "published",     default: false
     end
+
+    add_reference :jobs, :category, index:true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216183352) do
+ActiveRecord::Schema.define(version: 20160216183505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,5 +20,24 @@ ActiveRecord::Schema.define(version: 20160216183352) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
+
+  create_table "jobs", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.date     "starts_on"
+    t.date     "ends_on"
+    t.string   "location"
+    t.string   "contact_name"
+    t.string   "contact_phone"
+    t.string   "contact_email"
+    t.string   "company_name"
+    t.string   "secret"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "published",     default: false
+    t.integer  "category_id"
+  end
+
+  add_index "jobs", ["category_id"], name: "index_jobs_on_category_id", using: :btree
 
 end
