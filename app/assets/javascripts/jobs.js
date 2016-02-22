260b77e911e0c3e258f79e4b1fa66a8710f97d22
Holly-jolly <holly.haixin.l@gmail.com>
2016-02-22 09:10:42 -0800

    change jobs.js(coffeescript to javascript)

---
+++ app/assets/javascripts/jobs.js
@@ -0,0 +1,12 @@
+$(function(){
+  $("#job_category").change(function(){
+    var category_id;
+      category_id = $(this).val();
+        if (category_id) {
+          return window.location.href = "/categories/" + category_id;
+        } else {
+          return window.location.href = "/jobs";
+        };
+  });
+
+});
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,16 @@
 class CategoriesController < ApplicationController
   
   def index
+  # Firstly, need to show all 
+    @category = Category.all
+
     
   end
 
 
   def show
+    #showing specific user- category
+
     @c= Category.find(params[:id])
     @users = @c.users
     # @labels = Label.search_by_category(params[:id])
