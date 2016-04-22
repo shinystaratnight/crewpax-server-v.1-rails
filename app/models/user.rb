bd592654306b6519bae57092ca68f0168ea98a4e
ashleyyy <ashleyfisher@gmail.com>
2016-04-22 11:53:46 -0700

    corrected grammar of error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,9 @@ class User < ActiveRecord::Base
 
 
   validates :name, uniqueness: true, presence: true, length: {maximum: 64}
-  validates :phone, format:{with:/\d{10}/, message:"It must be a valid phone number"}, 
+  validates :phone, format:{with:/\d{10}/, message:" must be a valid phone number"}, 
   length: {maximum: 10}, if: "phone.present?"
-  validates :email,length: {maximum: 64}, format:{with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/},
+  validates :email,length: {maximum: 64}, format:{with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, message:" must be a valid email address"},
   if: "email.present?"
 
   
