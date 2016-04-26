891c6e338cddf7f7b1c60e87e492d1a96eb33854
ashleyyy <ashleyfisher@gmail.com>
2016-04-26 15:15:33 -0700

    adding eligibilities (roles in unions) works

---
+++ app/controllers/admin/eligibilities_controller.rb
@@ -0,0 +1,37 @@
+class Admin::EligibilitiesController < ApplicationController
+
+  def create
+    @eligibility = Eligibility.new(eligibility_params)
+    respond_to do |format|
+      if @eligibility.save
+        format.html{redirect_to @user}
+        format.json{render json: @eligibility}
+      else
+        format.html{render action: "new"}
+        format.json{render json: @eligibility.errors.full_messages, status: :unprocessable_entity}
+      end
+    end
+  end
+
+
+  def destroy
+    @eligibility = Eligibility.find(eligibility_params)
+    # respond_to do |format|
+    #   if @eligibility.save
+    #     format.html{redirect_to @user}
+    #     format.json{render json: @eligibility}
+    #   else
+    #     format.html{render action: "new"}
+    #     format.json{render json: @eligibility.errors.full_messages, status: :unprocessable_entity}
+    #   end
+    # end
+  end
+
+  private
+
+  def eligibility_params
+    params.require(:eligibility).permit(:union_id, :role_id)
+  end
+
+
+end
