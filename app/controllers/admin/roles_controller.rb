b2cfd19520bab898830ed0eb71f547115df5323c
ashleyyy <ashleyfisher@gmail.com>
2016-05-03 14:40:58 -0700

    edit role name works

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -7,9 +7,27 @@ class Admin::RolesController < ApplicationController
   def destroy
     @role = Role.find(params[:id])
 
-     @role.destroy
-
+    @role.destroy
   end
 
+  def update
+    @role = Role.find(params[:id])
+    respond_to do |format|
+      if @role.update_attributes(role_params)
+        format.html{redirect_to @user}
+        format.json{render json: @role}
+      else
+        format.html{render action: "edit"}
+        format.json{render json: @role.errors.full_messages, status: :unprocessable_entity}
+      end
+    end
+  end
+
+
+  private
+
+  def role_params
+    params.require(:role).permit(:name)
+  end
 
 end 
