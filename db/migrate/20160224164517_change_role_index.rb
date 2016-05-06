f3195cc6bf6daf738f59e1b361c23d36cc5594a6
Holly-jolly <holly.haixin.l@gmail.com>
2016-05-06 11:20:12 -0700

    comment out some of the migration files changes to avoid errors

--- db/migrate/20160224164517_change_role_index.rb
+++ db/migrate/20160224164517_change_role_index.rb
@@ -1,7 +1,7 @@
 class ChangeRoleIndex < ActiveRecord::Migration
   def change
     remove_index :jobs, column: :category_id
-    add_reference :jobs, :role, index: true
+    # add_reference :jobs, :role, index: true
     change_table :users do |t|
       t.remove :category_ids
       t.string :roles_ids, default: [], array:true
