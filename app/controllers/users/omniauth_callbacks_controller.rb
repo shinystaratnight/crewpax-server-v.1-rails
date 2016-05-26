f3436df7e6f381bdee2a3e58216a27fa2473286c
Holly-jolly <holly.haixin.l@gmail.com>
2016-05-26 12:52:21 -0700

    flash messages will be different for users who sign upwith credentials or facebook as they login using facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,11 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def facebook
-
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      #Flash message for users who sign up with credentials and later login with facebook
+      if @user.encrypted_password.present?
+        set_flash_message(:notice, :success_with_credentials_signup, :kind => "Facebook") if is_navigational_format?
+      else
+      #Flash message for users who sign up with facebook and later login with credentails       
+        set_flash_message(:notice, :success_with_facebook_signup, :kind => "Facebook") if is_navigational_format?
+      end
     else
       # if user is not created due to validation fails
       session["devise.facebook_data"] = request.env["omniauth.auth"]
