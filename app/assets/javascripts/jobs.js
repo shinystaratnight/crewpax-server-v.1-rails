fa043d83e8cdec4494d79cbd7f92afe8ac59d143
Holly-jolly <holly.haixin.l@gmail.com>
2016-05-30 15:21:03 -0700

    fix the job has filled on job board

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -58,6 +58,7 @@ $(function(){
   $("#job_posts_table").DataTable({
       responsive: true,
       "dom": '<"top"f>rt<"bottom"lip><"clear">',
+      "bSortClasses": false,
     //   fnRowCallback: function( nRow, aData, iDisplayIndex, iDisplayIndexFull ) {
     //     if (aData[3] == 'true'){
     //       $(nRow).css('background-color', '#d9534f')
