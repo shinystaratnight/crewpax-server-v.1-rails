94a1ee2ba297de7e69b4dec387116d9758d32c19
Holly-jolly <holly.haixin.l@gmail.com>
2016-05-31 14:25:56 -0700

    create a new column called recipient_id in the messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531161821) do
+ActiveRecord::Schema.define(version: 20160531212407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20160531161821) do
   create_table "messages", force: :cascade do |t|
     t.string  "content"
     t.integer "user_id"
+    t.integer "recipient_id"
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
