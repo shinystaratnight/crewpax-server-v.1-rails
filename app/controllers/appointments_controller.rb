b1258fdce542058e4007860ab96e2893caf893df
Holly-jolly <holly.haixin.l@gmail.com>
2016-06-13 11:01:25 -0700

    deleted uneeded code

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -40,19 +40,7 @@ class AppointmentsController < ApplicationController
     end
   end
 
-  # before_filter :authenticate_user!
 
-  # def toggle
-  #   appointment = current_user.appointments.where date: params[:date]
-
-  #   if appointment.exists?
-  #     appointment.first.destroy
-  #   else
-  #     current_user.appointments.create date: params[:date]
-  #   end
-
-  #   render nothing: true
-  # end
   private
 
   def appointment_params
