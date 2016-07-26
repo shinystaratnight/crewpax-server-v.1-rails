e69434256895fc42a59a8da163dc164a7fd945e6
Holly-jolly <holly.haixin.l@gmail.com>
2016-07-25 17:00:18 -0700

    when sponsors images are uploaded, file will be renamed and will be assigned an unqiue id

--- app/uploaders/sponsor_uploader.rb
+++ app/uploaders/sponsor_uploader.rb
@@ -11,8 +11,23 @@ class SponsorUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg gif png)
   end
 
+  def filename
+    if original_filename.present?
+      extension = File.extname(original_filename).to_s
+      "sponsor_image_sponsor_random_id_"+"#{secure_token(10)}"+ extension 
+    end
+  end
+
+  process :resize_to_fit =>[240, 240]
+
   version :thumb do
      process :resize_to_fit => [90, 90]
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
   end
 
 end
