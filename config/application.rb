48964c37d987101dad464d69952d2021a0947ba9
Noam <noam.ascher@gmail.com>
2017-04-16 21:25:33 +0000

    temp commit to fix gitignore

--- config/application.rb
+++ config/application.rb
@@ -49,15 +49,3 @@ module Bcpax
 
   end
 end
-
-
-
-# #webpush things
-# vapid_key = Webpush.generate_key
-
-# # Save these in our application server settings
-# vapid_key.public_key
-# # => "BC1mp...HQ="
-
-# vapid_key.private_key
-# # => "XhGUr...Kec"
