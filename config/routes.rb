34ab9ef2cad0fc07a818e3ec20c3b15330ffef29
Noam <noam.ascher@gmail.com>
2017-04-16 20:11:23 +0000

    sms set up with trial twilio number

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Bcpax::Application.routes.draw do
 
   root 'home#index'
 
-  #twilio route
-  post 'twilio/voice' => 'twilio#voice'
+  #twilio route - if needed
+  # post 'twilio/voice' => 'twilio#voice'
 
 end
