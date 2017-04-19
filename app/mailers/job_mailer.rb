80946fa79770d3889cb111ae4ce4331d48935c54
Noam <noam.ascher@gmail.com>
2017-04-19 02:29:40 +0000

    job url domain flexibility on mailer

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -11,8 +11,8 @@ class JobMailer < ActionMailer::Base
   # notify users of the job posting based on their notification settings
   def notification(job, email)
     @job = job
-    @hostname = root_url
-    @bodystring = "#{@job.name}\n\nLocation: #{@job.location}\n\nDescription: #{@job.description}\n\nStarts on: #{@job.starts_on}\n\nLink: #{@hostname}:3000/jobs/#{@job.id}"
+    #@hostname = root_url
+    @bodystring = "#{@job.name}\n\nLocation: #{@job.location}\n\nDescription: #{@job.description}\n\nStarts on: #{@job.starts_on}\n\nLink: #{root_url}/jobs/#{@job.id}"
     mail to: email, subject: "New #{@job.role.name} Job Posted To Crewcall", body: @bodystring
   end
 
