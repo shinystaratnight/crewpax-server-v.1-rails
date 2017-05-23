3ab5cdbd543dc2ccc06ec92d086610e5f5ea6a29
Noam <noam.ascher@gmail.com>
2017-05-23 19:50:54 +0000

    added image info storing in database for profile pics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -105,8 +105,13 @@ class UsersController < ApplicationController
 
       if user_params[:image].present?
         if Rails.env.production? #store images in dropbox
-          client = dropbox_client
           @user.update(image: user_params[:image])
+          client = dropbox_client
+
+          #in production environment, store image info (for url)
+          @file = user_params[:image]
+          @user.image = @file
+          @user.save
           format.json{render json: @user}
         else
           @file = user_params[:image] #in development environment, store images locally
